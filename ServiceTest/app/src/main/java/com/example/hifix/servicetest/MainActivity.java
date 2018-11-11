e3e8ceecb75fcb36816c40ee152036181afd1d87
hifix <928675426@qq.com>
2018-11-11 16:35:18 +0800

    日常提交

--- ServiceTest/app/src/main/java/com/example/hifix/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/hifix/servicetest/MainActivity.java
@@ -1,13 +1,38 @@
 package com.example.hifix.servicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+    private Button startService;
+    private Button stopService;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent); // 启动服务
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent); // 停止服务
+                break;
+            default:
+                break;
+        }
     }
 }
